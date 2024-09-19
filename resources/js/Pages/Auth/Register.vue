7d1be6e82cd14dba142cd708f4dd3a33a41a67e2
DaryllCulas <culasdaryll99@gmail.com>
2024-09-20 02:13:58 +0800

    Register form handler

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -4,23 +4,57 @@ import Title from "../../Components/Title.vue";
 import TextLink from "../../Components/TextLink.vue";
 import InputField from "../../Components/InputField.vue";
 import PrimaryBtn from "../../Components/PrimaryBtn.vue";
+import ErrorMessages from "../../Components/ErrorMessages.vue";
+import { useForm } from "@inertiajs/vue3";
+
+
+const form = useForm({
+  name: "",
+  email: "",
+  password: "",
+  password_confirmation: "",
+});
+
+// TODO: Apply Toast when the registration is successful
+const submit = () => {
+  form.post(route("register"), {
+    onFinish: () => form.reset("password", "password_confirmation"),
+  });
+};
 </script>
 
 <template>
   <Container class="w-1/2">
     <div class="mb-8 text-center">
       <Title>Register a new account</Title>
-      <p>Already have an account? <TextLink routeName="home" label="Login" /></p>
-    </div>
-    <form class="space-y-6">
-      <InputField label="Name" icon="id-badge" />
-      <InputField label="Email" icon="at" type="email" />
-      <InputField label="Password" icon="key" type="email" />
-      <InputField label="Confirm Password" icon="key" type="password" />
-      <p class="text-slate-500 text-sm dark:text-slate-400">
-        By creating an account, you agree to our terms of service and Privacy Policy.
+      <p>
+        Already have an account?
+        <TextLink routeName="home" label="Login" />
       </p>
-      <PrimaryBtn>Register</PrimaryBtn>
+    </div>
+
+    <form @submit.prevent="submit" class="space-y-6">
+      <InputField label="Name" icon="id-badge" v-model="form.name" />
+
+      <InputField label="Email" icon="at" v-model="form.email" />
+
+      <InputField label="Password" type="password" icon="key" v-model="form.password" />
+
+      <InputField
+        label="Confirm Password"
+        type="password"
+        icon="key"
+        v-model="form.password_confirmation"
+      />
+
+      <!-- Errors messages -->
+      <ErrorMessages :errors="form.errors" />
+
+      <p class="text-slate-500 text-sm dark:text-slate-400">
+        By creating an account, you agree to our Terms of Service and Privacy Policy.
+      </p>
+
+      <PrimaryBtn :disabled="form.processing">Register</PrimaryBtn>
     </form>
   </Container>
 </template>

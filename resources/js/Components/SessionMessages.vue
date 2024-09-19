34becc4eda11091bee8a8c666c4e22a4bdbf5339
DaryllCulas <culasdaryll99@gmail.com>
2024-09-20 07:57:04 +0800

    Email Verification

---
+++ resources/js/Components/SessionMessages.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({
+  statusMessage: String,
+});
+</script>
+<template>
+  <div v-if="statusMessage" class="mb-4 font-medium text-sm text-green-500">
+    {{ statusMessage }}
+  </div>
+</template>
--- routes/auth.php
+++ routes/auth.php
@@ -1,17 +1,15 @@
 <?php
 use App\Http\Controllers\Auth\AuthenticateController;
+use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function(){
-
+Route::middleware('guest')->group(function () {
 
     // -------------- Register ------------------//
 
     Route::get('/register', [RegisterController::class, 'create'])->name('register');
     Route::post('/register', [RegisterController::class, 'store']);
-
-
 
     // -------------- Login ------------------//
     Route::get('/login', [AuthenticateController::class, 'create'])->name('login');
@@ -19,11 +17,23 @@ Route::middleware('guest')->group(function(){
 
 });
 
-
-Route::middleware('auth')->group(function(){
+Route::middleware('auth')->group(function () {
 
     // -------------- Logout ------------------//
-Route::post('/logout', [AuthenticateController::class, 'destroy'])->name('logout');
+    Route::post('/logout', [AuthenticateController::class, 'destroy'])->name('logout');
 
+    // -------------- Verify Email ------------------//
+    route::get('/email/verify', [EmailVerificationController::class, 'notice'])
+    ->middleware('auth')
+    ->name('verification.notice');
+
+    // -------------- Handle Email ------------------//
+    Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'handler'])
+    ->middleware('signed')
+    ->name('verification.verify');
+
+    Route::post('/email/verification-notification', [EmailVerificationController::class, 'resend'])
+    ->middleware('throttle:6,1')
+    ->name('verification.send');
 
 });
